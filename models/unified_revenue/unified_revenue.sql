6158427cccf36d6fd9ba97eb6ef692ecc5c1a8b5
Vanja Nevajdic <vanja.nevajdic@solflare.com>
2026-02-26 16:50:21 +0100

    aggregate by type and remove tokens from unified_revenue

--- models/unified_revenue/unified_revenue.sql
+++ models/unified_revenue/unified_revenue.sql
@@ -3,8 +3,31 @@
     materialized = 'table'
 ) }}
 
-select date, fee_type, fee_source, token, usd_price, amount, usd_amount
-from {{ ref('product_revenue') }}
-union
-select date, fee_type, fee_source, token, usd_price, amount, usd_amount
-from {{ ref('staking_revenue') }}
+with base as (
+    select date, fee_type, token, usd_price, amount, usd_amount
+    from {{ ref('product_revenue') }}
+    union all
+    select date, fee_type, token, usd_price, amount, usd_amount
+    from {{ ref('staking_revenue') }}
+),
+aggregated as (
+    select
+        date,
+        fee_type,
+        sum(case when token in ('sol', 'wsol') then amount else 0 end) as sol_amount,
+        max(case when token in ('sol', 'wsol') then usd_price end) as sol_price_usd,
+        sum(case when token in ('sol', 'wsol') then usd_amount else 0 end) as sol_amount_usd,
+        sum(case when token = 'usdc' then amount else 0 end) as usdc_amount,
+        sum(usd_amount) as total_usd_amount 
+    from base
+    group by date, fee_type
+)
+select
+    date,
+    fee_type as type,
+    sol_amount,
+    sol_price_usd,
+    sol_amount_usd,
+    usdc_amount,
+    total_usd_amount
+from aggregated
